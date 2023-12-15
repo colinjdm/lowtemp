85a1bf7559784b5fe55edcb43407852c06fbce12
colinjdm <colinjdmitchell@gmail.com>
2023-12-15 12:02:36 -0600

    cleaning up lowtemp

--- lowtemp.py
+++ lowtemp.py
@@ -1,7 +1,5 @@
 # displays low temperature for the next 24 hours
-import json
 import requests
-import sys
 
 # prettyprint for dict formatting
 from pprint import pprint
@@ -10,26 +8,27 @@ from pprint import pprint
 def main():
     r = requests.get('https://api.weather.gov/points/32.6983,-85.6205')
     #pprint(r.json())
-
     forecast = requests.get(r.json()['properties']['forecastHourly'])
-
     periods = (forecast.json()['properties']['periods'])
-
     #pprint(periods)
 
-    for key in periods:
+    for i, key in enumerate(periods):
         temp = key['temperature']
         time = key['endTime']
-        snowflakes = coldness(temp)
-        print(f"At {time} the temperature will be {temp} {snowflakes}")
+        print(f"At {time} the temperature will be {temp} {snowflakes(temp)}")
+        
+        # number of hours to stop at
+        # default api shows about 5 days
+        limit = 24
+        if i == limit:
+            break
 
 
-def coldness(t):
+def snowflakes(t):
     string = f""
-    snowflakes = t // 10
-    for i in range(snowflakes):
+    temp = t // 10
+    for i in range(temp):
         string = string + "*"
-
     return(string)
 
 
