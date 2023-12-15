e5e7d7d9f0f8c155ce959c9ac865ddcf222b087f
colinjdm <colinjdmitchell@gmail.com>
2023-12-15 12:28:50 -0600

    added regex to find time

--- lowtemp.py
+++ lowtemp.py
@@ -1,4 +1,5 @@
 # displays low temperature for the next 24 hours
+import re
 import requests
 
 # prettyprint for dict formatting
@@ -15,14 +16,27 @@ def main():
     for i, key in enumerate(periods):
         # enumerate tracks the number of loops as 'i'
         temp = key['temperature']
-        time = key['endTime']
-        print(f"At {time} the temperature will be {temp} {snowflakes(temp)}")
+        time = key['startTime']
+        hour = pull_time(time)
+        print(f"At {hour}:00 the temperature will be {temp} {snowflakes(temp)}")
+        #print(f"{key['detailedForecast']}")
         
+        # stops at noon
+        if hour == '12':
+            break
         # number of hours to stop at
         # default api shows about 5 days
         limit = 24
         if i == limit:
             break
+
+
+def pull_time(s):
+    hour = re.search(r"T([0-9][0-9]):", s)
+    # convert the string into an integer
+    hour = hour[0].replace(":", "")
+    hour = hour.replace("T", "")
+    return int(hour)
 
 
 def snowflakes(t):
