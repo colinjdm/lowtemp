bcfb0637f8b118672a977bada42173cede1fa0fa
colinjdm <colinjdmitchell@gmail.com>
2024-04-25 12:00:16 -0500

    switched units and removed conversion

--- openweathermap.py
+++ openweathermap.py
@@ -1,36 +1,34 @@
 import datetime
 import requests
+import sys
 
 from pprint import pprint
 
 API_key = 'e1adbe8a851f3f58b8d9dcd898aafacd'
-lat = 32.73515023952092
-lon = -85.57369718339207
 
 def get_weather(lat, lon):
-    r = requests.get(f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API_key}')
-    hourly_weather = r.json()['hourly']
+    try:
+        r = requests.get(f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API_key}&units=imperial')
+        hourly_weather = r.json()['hourly']
+    except Exception:
+        sys.exit('API is not responding')
 
     hourly_temp = []
     hourly_time = []
     for i, key in enumerate(hourly_weather):
-        #print(key['dt'])
+        #pprint(key['dt'])
 
         # unix time conversion
         time = (datetime.datetime.fromtimestamp(key['dt']))
         
-        # pull just the hour from the datetime string
-        # might move this into app.py so I can keep this info moving forward
-        # time = datetime.datetime.strftime(time, '%H')
+        # pull whatever info is needed from the datetime string
+        # currently --> year, month date, time (24hr)
+        time = datetime.datetime.strftime(time, '%Y, %B %d, %H:00')
 
         hourly_time.append(time)
+        hourly_temp.append(round(key['temp']))
 
-        # kelvin to fahrenheit conversion
-        hourly_temp.append(round(((key['temp'] - 273.15) * (9 / 5)) + 32))
-
-        # print debug
-    for i in range(len(hourly_temp)):
-        print(f'{hourly_time[i]}, {hourly_temp[i]} degrees')
+    return(hourly_time, hourly_temp)
 
 
 if __name__ == "__main__":
