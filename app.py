8ac4e10e45c418efee0ef08c95be069eb1c166c8
colinjdm <colinjdmitchell@gmail.com>
2024-04-25 12:01:02 -0500

    removing old code

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from colorama import Fore
 from dotenv import load_dotenv
 # flask
 from flask import Flask, redirect, render_template, request
+from openweathermap import get_weather
 # prettyprint for dict formatting
 from pprint import pprint
 
@@ -30,62 +31,28 @@ def main():
         return render_template("index.html")
     
     elif request.method == "POST":
+        # get search term from html
         location = request.form.get('search')
         # initial request to determine location based on lat/long
         lat, lng, address = geocode(location)
-        #print(f"Lat:      {lat}")
-        #print(f"Long:    {lng}")
-        #print(f"Location: {address}")
-        # suppress errors when the API isn't responding
-        try:
-            # first request to obtain location URL
-            r = requests.get(f'https://api.weather.gov/points/{lat},{lng}')
-            # second request to obtain forecast
-            forecast = requests.get(r.json()['properties']['forecastHourly'])
-            periods = (forecast.json()['properties']['periods'])
-        except Exception:
-            sys.exit('API is not responding')
+        web_times, web_temps = get_weather(lat, lng)
+        pprint(web_times)
+        pprint(web_temps)
 
+        # placeholder
+        web_precips = []
 
-        # define empty lists where times, temps, and precip can be stored
-        web_temps = []
-        web_times = []
-        web_precips = [] 
+        # hour = pull_time(time)
+        # hour, meridiem = get_meridiem(hour)
+        # web_times.append(f'{hour} {meridiem}')
 
-        for i, key in enumerate(periods):
-            # enumerate will track the number of loops as 'i'
-            temp = key['temperature']
-            time = key['startTime']
-            fc = key['shortForecast']
-            precipitation = key['probabilityOfPrecipitation']['value']
-
-            # append web lists with appropriate values every loop
-            # make sure lists have same number of values
-            web_temps.append(temp)
-            web_precips.append(precipitation)
-
-            if precipitation < 10:
-                rain = ''
-                fc = ''
-            else:
-                rain = f"\U0001F4A7 {precipitation}%"
-
-            hour = pull_time(time)
-            hour, meridiem = get_meridiem(hour)
-            web_times.append(f'{hour} {meridiem}')
-
-            # the variable {fc} can be added later to include a short forecast
-            # print(f"{hour:2}:00 {meridiem} {get_color(temp) + graph(temp) + Fore.RESET}   {temp}\u00B0   {Fore.BLUE + rain + Fore.RESET}")
-            # print(f"{key['detailedForecast']}")
-            
-            # stops displaying temps at noon as long as 6 hours have already been displayed
-            if hour == 12 and meridiem == 'pm' and i > 6:
-                break
+        # the variable {fc} can be added later to include a short forecast
+        # print(f"{hour:2}:00 {meridiem} {get_color(temp) + graph(temp) + Fore.RESET}   {temp}\u00B0   {Fore.BLUE + rain + Fore.RESET}")
+        # print(f"{key['detailedForecast']}")
         
-        # test prints
-        # pprint(web_temps)
-        # pprint(web_times)
-        # pprint(web_precips)
+        # stops displaying temps at noon as long as 6 hours have already been displayed
+        # if hour == 12 and meridiem == 'pm' and i > 6:
+        #    break
 
         return render_template("weather.html", address=address, web_temps=web_temps, web_times=web_times, web_precips=web_precips)
 
