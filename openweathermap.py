fd9898d74e6a40b92c0f94a43c245c36e21aca6a
colinjdm <colinjdmitchell@gmail.com>
2024-04-25 16:06:29 -0500

    Added precipitation chance and moon phase

--- openweathermap.py
+++ openweathermap.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import os
 import requests
 import sys
@@ -12,29 +13,41 @@ from pprint import pprint
 load_dotenv()
 
 def get_weather(lat, lon):
+
     api_key = os.getenv('API_key')
     try:
         r = requests.get(f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={api_key}&units=imperial')
+        weather = r.json()
         hourly_weather = r.json()['hourly']
     except Exception:
         sys.exit('API is not responding')
 
+    # moon phase
+    moon_phase = weather['daily'][0]['moon_phase']
+    #print(f'Moon phase: {moon_phase}')
+
     hourly_temp = []
     hourly_time = []
+    hourly_precip = []
+
     for i, key in enumerate(hourly_weather):
-        #pprint(key['dt'])
+
+        # hourly precipitation chance
+        precip = weather['hourly'][i]['pop'] * 100
+        #print(weather['hourly'][i])
+        hourly_precip.append(precip)
 
         # unix time conversion
         time = (datetime.datetime.fromtimestamp(key['dt']))
-        
         # pull whatever info is needed from the datetime string
-        # currently --> year, month date, time (24hr)
-        time = datetime.datetime.strftime(time, '%Y, %B %d, %H:00')
-
+        # currently --> day, month date, time (24hr)
+        time = datetime.datetime.strftime(time, '%A, %B %d, %H:00')
         hourly_time.append(time)
-        hourly_temp.append(round(key['temp']))
 
-    return(hourly_time, hourly_temp)
+        # hourly temperature
+        hourly_temp.append(round(key['temp']))
+    print(f'{len(hourly_time)},{len(hourly_temp)},{len(hourly_precip)}')
+    return(hourly_time, hourly_temp, hourly_precip)
 
 
 if __name__ == "__main__":
